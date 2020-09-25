52fdc673b13da8493651eb7fbee545454edc2abe
aleksandrakostina <60540168+aleksandrakostina@users.noreply.github.com>
2020-09-25 18:10:44 +0300

    feat: add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="wrapper header__wrapper">
+        <h2 className="header__title">Список задач</h2>
+      </div>    
+    </header>
+  )
+}
+
+export default Header;
